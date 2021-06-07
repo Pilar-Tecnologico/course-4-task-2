d7e06b7c1c157a95b9f0bb23fb10f8d823a8bb0d
Lucas Bracco <braccolucas1393@gmail.com>
2021-06-07 13:31:16 -0300

    ADD: GET /mars/manifest/:roverName

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -1,9 +1,24 @@
 const axios = require('axios').default;
 const querystring = require('querystring');
 const apikey = process.env.API_KEY;
+// rover_name: Curiosity
 
 async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
+    const roverName = req.params.roverName;
+    const nasaKey = querystring.stringify({api_key: apikey});
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?${nasaKey}`)
+        .then((response) => {
+            const resData =  response.data.photo_manifest;
+            resData['last_manifest'] = resData.photos.pop();
+            delete resData.photos;
+            res.status(200).json(resData);
+        })
+        .catch(err => {
+            res.status(400).json({
+                code: 'bad_request',
+                message: 'Bad request. Please check your parameters values'
+            });
+        })
 };
 
 module.exports = {getManifest};
--- routes/mars.js
+++ routes/mars.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const marsController = require('../controllers/mars.controller');
 
 router.get('/manifests/:roverName', function(req, res) {
-  //COMPLETE WITH YOUR CODE
+  marsController.getManifest(req, res);
 });
 
 module.exports = router;
