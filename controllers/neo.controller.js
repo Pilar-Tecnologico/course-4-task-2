24488035cef57e94bf5b594bf430ee29c390280f
Marcelo R <marcelofabio01@yahoo.com.ar>
2021-06-07 17:58:43 -0300

    Modificaciones realizadas

--- controllers/neo.controller.js
+++ controllers/neo.controller.js
@@ -4,6 +4,23 @@ const apikey = process.env.API_KEY;
 
 async function getNeoFeed(req, res){
     //COMPLETE WITH YOUR CODE
+    const now = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`;
+    const paramsQuery = querystring.stringify({start_date: now, end_date: now, api_key:apikey});
+    axios.get(`https://api.nasa.gov/neo/rest/v1/feed?${paramsQuery}`)
+        .then((response) =>{
+            console.log(response.data.url);
+            const data = response.data;
+            res.json(data);
+            delete data.links;
+            res.status(200).json(data);     
+        })
+        .catch((err) => {
+            console.log(error.message);
+            res.status(500).json({
+              code: "internal_server_error",
+              message: "Something went wrong",
+            });
+        });
 };
 
 module.exports = {getNeoFeed};
