408f01ba9a6b69ff3620e5455f545b52212b265d
Lucas Cortés <c9.lucas@gmail.com>
2021-06-09 21:37:43 -0300

    Task 2 completed

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -4,6 +4,28 @@ const apikey = process.env.API_KEY;
 
 async function getManifest(req, res){
     //COMPLETE WITH YOUR CODE
+    const roverName = req.params.roverName;
+    axios
+    .get(
+        `https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?api_key=${apikey}`
+    ).then((response) => {
+        const resData =  response.data.photo_manifest;
+        
+        const last_manifiest = response.data.photo_manifest.photos[
+            response.data.photo_manifest.photos.length - 1
+        ];
+
+        delete resData.photos;
+
+        resData.last_manifest = last_manifiest;
+        res.status(200).json(resData);
+    })
+    .catch((err) => {
+        res.status(400).json({
+            code: 'bad_request',
+            message: 'Bad request. Please check your parameters values'
+        });
+    })
 };
 
 module.exports = {getManifest};
--- controllers/neo.controller.js
+++ controllers/neo.controller.js
@@ -1,9 +1,29 @@
 const axios = require('axios').default;
 const querystring = require('querystring');
 const apikey = process.env.API_KEY;
+const { DateTime } = require("luxon");
+
 
 async function getNeoFeed(req, res){
-    //COMPLETE WITH YOUR CODE
+        //COMPLETE WITH YOUR CODE
+    const today = DateTime.now().toISODate();
+    axios
+    .get(
+        `https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&end_date=${today}&api_key=${apikey}`
+    )
+    .then((response) => {
+        const res_obj = {
+            element_count: response.data.element_count,
+            near_earth_objects: response.data.near_earth_objects,
+        };
+        res.status(200).json(res_obj);
+    })
+    .catch((err) => {
+        res.status(500).json({
+            code: 'internal_server_error',
+            message: 'Something went wrong',
+        });
+    });
 };
 
 module.exports = {getNeoFeed};
