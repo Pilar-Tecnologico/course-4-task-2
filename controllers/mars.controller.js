b6a44a13c9154bb4ffc9bf3fed09fb8c2f5f7e95
javpintos <jpintos234@gmail.com>
2021-06-10 10:52:26 -0300

    first commit

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -4,6 +4,16 @@ const apikey = process.env.API_KEY;
 
 async function getManifest(req, res){
     //COMPLETE WITH YOUR CODE
+    const params = {
+        roverName: req.params.roverName
+    };
+    const axiosParams = querystring.stringify({api_key: apiKey, ...params})
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/manifests/:roverName${axiosParams}`)
+    .then((response) => {
+        res.json(response.data)
+    }).catch(err => {
+        res.status(500).json(err);
+    })
 };
 
 module.exports = {getManifest};
--- routes/mars.js
+++ routes/mars.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const marsController = require("../controllers/mars.controller");
 
-router.get('/manifests/:roverName', function(req, res) {
+router.get('/mars/manifest/:roverName', function(req, res) {
   //COMPLETE WITH YOUR CODE
+  marsController.getManifest(req, res);
 });
 
 module.exports = router;
