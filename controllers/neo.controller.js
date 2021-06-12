fdea7a9762bfcf200c69695537121c446954434f
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-06-11 22:08:30 -0300

    add endpoints :: /mars/manifest/:roverName and :: /neo/feed

--- controllers/neo.controller.js
+++ controllers/neo.controller.js
@@ -1,9 +1,28 @@
 const axios = require('axios').default;
 const querystring = require('querystring');
 const apikey = process.env.API_KEY;
+const { DateTime } = require("luxon");
 
 async function getNeoFeed(req, res){
     //COMPLETE WITH YOUR CODE
+    const today = DateTime.now().toISODate();
+    const params = querystring.stringify({
+        start_date: today,
+        end_date: today,
+        api_key: apikey
+    });    
+    axios.get(`https://api.nasa.gov/neo/rest/v1/feed?${params}`)
+        .then((response) => {
+            const data = response.data;
+            delete data.links;
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(500).json({
+                "code": "internal_server_error",
+                "message": "Something went wrong"
+            });
+        });
 };
 
 module.exports = {getNeoFeed};
