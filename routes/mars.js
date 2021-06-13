d51628ff9b6539e72b1f4a0e9ce824baeaa54b9e
Javier Pintos <jpintos234@gmail.com>
2021-06-12 23:44:50 -0300

    manifests route corrected

--- routes/mars.js
+++ routes/mars.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const marsController = require("../controllers/mars.controller");
 
-router.get('/manifest/:roverName', function(req, res) {
+router.get('/manifests/:roverName', function(req, res) {
   //COMPLETE WITH YOUR CODE
   const roverName = req.params.roverName;
   marsController.getManifest(req, res);
