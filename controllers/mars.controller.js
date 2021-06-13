4d2d6016fa7131a583596a2ed90ac0fb06a46a48
Ignacio Neira <ineira16@gmail.com>
2021-06-12 23:27:02 -0300

    First point done

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -4,6 +4,18 @@ const apikey = process.env.API_KEY;
 
 async function getManifest(req, res){
     //COMPLETE WITH YOUR CODE
+    const roverName = req.params.roverName;
+    const axiosParams = querystring.stringify({api_key: apikey})
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?${axiosParams}`)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch(err =>{
+            res.status(400).json({
+                code: "bad_request",
+                message: "Bad request. Please check your parameters values"
+            });
+        })
 };
 
 module.exports = {getManifest};
