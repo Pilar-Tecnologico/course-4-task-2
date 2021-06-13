4981a454818b3b8abc6fb28e79272e2aca53c178
Ignacio Neira <ineira16@gmail.com>
2021-06-12 23:41:09 -0300

    Second point done and Task Complete

--- controllers/neo.controller.js
+++ controllers/neo.controller.js
@@ -4,6 +4,24 @@ const apikey = process.env.API_KEY;
 
 async function getNeoFeed(req, res){
     //COMPLETE WITH YOUR CODE
+    const params = {
+        start_date: req.query.start_date,
+        end_date: req.query.end_date
+    };
+    const axiosParams = querystring.stringify({api_key: apikey, ...params})
+    axios.get(`https://api.nasa.gov/neo/rest/v1/feed?${axiosParams}`)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch(err =>{
+            res.status(500).json({
+                code: "internal_server_error",
+                message: "Something went wrong"
+            });
+        })
 };
 
 module.exports = {getNeoFeed};
+
+
+
