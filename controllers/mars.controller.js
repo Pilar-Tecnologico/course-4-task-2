12fce5b4cd9ed87d8cde114a32ab4172abe6f63c
AldanaBaurer <aldana.baurer@sponsorize.ch>
2021-06-14 22:51:52 -0300

    Task 2 - Ready

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -3,7 +3,25 @@ const querystring = require('querystring');
 const apikey = process.env.API_KEY;
 
 async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
+    const params = req.params.roverName;
+    const axiosParams = querystring.stringify({api_key: apikey})
+    
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/manifests/${params}?${axiosParams}`)
+    .then((response) => {
+        const data = response.data.photo_manifest;
+        const lastManifest = data.photos.pop();
+        delete data.photos;
+      
+        data.lastManifest = lastManifest;
+       
+        res.status(200).json(data);
+    })
+    .catch(err => {
+        res.status(400).json({
+            code: 'bad_request',
+            message: 'Bad request. Please check your parameters values'
+        })
+    })
 };
 
 module.exports = {getManifest};
--- controllers/neo.controller.js
+++ controllers/neo.controller.js
@@ -1,9 +1,30 @@
 const axios = require('axios').default;
 const querystring = require('querystring');
+const { DateTime } = require("luxon");
 const apikey = process.env.API_KEY;
 
 async function getNeoFeed(req, res){
-    //COMPLETE WITH YOUR CODE
+
+    const todaysDate = DateTime.now().toISODate();
+    const query = {
+        start_date: todaysDate,
+        end_date: todaysDate
+    };
+    const axiosParams = querystring.stringify({api_key: apikey, ...query})
+
+    axios.get(`https://api.nasa.gov/neo/rest/v1/feed?${axiosParams}`)
+    .then((response) => {
+        const data = response.data;
+        delete data.links;
+       
+        res.status(200).json(data);
+    })
+    .catch(err => {
+        res.status(500).json({
+            "code": "internal_server_error",
+            "message": "Something went wrong"
+        })
+    })
 };
 
 module.exports = {getNeoFeed};
