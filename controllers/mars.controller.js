0805e54db7cadb0814f7cab0428a22aed5b39a73
Lucas Bracco <braccolucas1393@gmail.com>
2021-06-30 11:50:38 -0300

    ADD: rovers' photos
    	-endpoint get rovers photos.

--- controllers/mars.controller.js
+++ controllers/mars.controller.js
@@ -1,16 +1,18 @@
+// rovers' names: Curiosity, Opportunity, Spirit.
+
 const axios = require('axios').default;
 const querystring = require('querystring');
 const apikey = process.env.API_KEY;
-// rover_name: Curiosity
 
 async function getManifest(req, res){
+    camera = req.query.camera;
     const roverName = req.params.roverName;
     const nasaKey = querystring.stringify({api_key: apikey});
     axios.get(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?${nasaKey}`)
         .then((response) => {
             const resData =  response.data.photo_manifest;
-            resData['last_manifest'] = resData.photos.pop();
-            delete resData.photos;
+            // resData['last_manifest'] = resData.photos.pop();
+            // delete resData.photos;
             res.status(200).json(resData);
         })
         .catch(err => {
@@ -21,4 +23,21 @@ async function getManifest(req, res){
         });
 };
 
-module.exports = {getManifest};
+async function getRoversPhotos(req, res){
+    const sol = req.query.sol
+    const camera = req.query.camera;
+    const roverName = req.params.roverName;
+    const nasaKey = querystring.stringify({api_key: apikey});
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&camera=${camera}&${nasaKey}`)
+        .then((response) => {
+            res.status(200).json(response.data);
+        })
+        .catch(err => {
+            res.status(400).json({
+                code: 'bad_request',
+                message: 'Bad request. Please check your parameters values'
+            });
+        });
+};
+
+module.exports = {getManifest, getRoversPhotos};
--- routes/mars.js
+++ routes/mars.js
@@ -5,5 +5,8 @@ const marsController = require('../controllers/mars.controller');
 router.get('/manifests/:roverName', function(req, res) {
   marsController.getManifest(req, res);
 });
+router.get('/:roverName/photos', function(req, res) {
+  marsController.getRoversPhotos(req, res);
+});
 
 module.exports = router;
