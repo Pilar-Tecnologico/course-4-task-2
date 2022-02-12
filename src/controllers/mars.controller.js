5ffb4c7b89d591c9e3f39424c1cbe7e30ddc033f
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2022-02-12 16:40:09 -0300

    - MODIFICATIONS: routings - To Endpoint "Test": GET /mars/manifest/:roverName & GET /neo/feed.

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -4,6 +4,8 @@ const {hostname, apikey} = config.get('services.nasa');
 
 async function getManifest(req, res){
     //COMPLETE WITH YOUR CODE
+    //### GET :: /mars/manifest/:roverName --> 
+    res.json('Endpoint test /mars/manifest ok!');
 };
 
 module.exports = {getManifest};
--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -4,6 +4,8 @@ const {hostname, apikey} = config.get('services.nasa');
 
 async function getNeoFeed(req, res){
     //COMPLETE WITH YOUR CODE
+    //### GET :: /neo/feed. --> 
+    res.json('Endpoint test /Neo/Feed ok!');
 };
 
 module.exports = {getNeoFeed};
