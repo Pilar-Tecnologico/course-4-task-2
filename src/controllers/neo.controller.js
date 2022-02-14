3fb3c79892b9f72f7e4c3c86aa3f67c7450eb1f6
Ezequiel carrizo <ezequielce490@gmail.com>
2022-02-14 17:05:54 -0300

    neo task complete

--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -1,9 +1,30 @@
-const axios = require('axios').default;
-const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const axios = require("axios").default;
+const config = require("config");
+const { hostname, apikey } = config.get("services.nasa");
+const luxon = require("luxon");
 
-async function getNeoFeed(req, res){
-    //COMPLETE WITH YOUR CODE
-};
+async function getNeoFeed(req, res) {
+  //COMPLETE WITH YOUR CODE
+  try {
+    const now = luxon.DateTime.now().toISODate();
+    const queryString = new URLSearchParams({
+      start_date: now,
+      end_date: now,
+      api_key: apikey,
+    });
+    const response = await axios.get(
+      `${hostname}/neo/rest/v1/feed?${queryString.toString()}`
+    );
 
-module.exports = {getNeoFeed};
+    const { links, ...data } = response.data;
+
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      code: "internal_server_error",
+      message: "Somethin went wrong",
+    });
+  }
+}
+
+module.exports = { getNeoFeed };
