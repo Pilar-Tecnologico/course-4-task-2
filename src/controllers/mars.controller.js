ffb8baf35535d5c4331706d366ca9b16877797a4
Ezequiel carrizo <ezequielce490@gmail.com>
2022-02-14 13:24:19 -0300

    mars task complete

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -1,9 +1,28 @@
-const axios = require('axios').default;
-const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const axios = require("axios").default;
+const config = require("config");
+const { hostname, apikey } = config.get("services.nasa");
+const marsSchema = require("./schemas/marsSchema");
+const joi = require("joi");
 
-async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
-};
+async function getManifest(req, res) {
+  //COMPLETE WITH YOUR CODE
+  try {
+    const { roverName } = req.params;
+    joi.assert(roverName, marsSchema);
+    const response = await axios.get(
+      `${hostname}/mars-photos/api/v1/manifests/${roverName}?api_key=${apikey}`
+    );
+    const { photos, ...resData } = response.data.photo_manifest;
+    const manifest = { ...resData, last_manifest: photos.pop() };
+    res.json(manifest);
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        code: "bad_request",
+        message: "Bad request. Please check your parameters values",
+      });
+  }
+}
 
-module.exports = {getManifest};
+module.exports = { getManifest };
