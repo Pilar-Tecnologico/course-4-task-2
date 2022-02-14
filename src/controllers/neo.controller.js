e22a89bf72411ef7b02f543f7e66bc4f7199e2a2
Leonel Moreno <leonel_moreno99@hotmail.com>
2022-02-14 01:12:08 -0300

    endpoint 2 getNeoFeed completed

--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -1,9 +1,31 @@
-const axios = require('axios').default;
-const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const axios = require("axios").default;
+const config = require("config");
+const Joi = require("joi");
+const { hostname, apikey } = config.get("services.nasa");
+const { DateTime } = require("luxon");
+const { neoSchema } = require("./schemas/nasa.schema");
 
-async function getNeoFeed(req, res){
-    //COMPLETE WITH YOUR CODE
-};
+const getNeoFeed = async (req, res) => {
+  try {
+    const now = DateTime.now().toISODate();
+    const params = {
+      start_date: now,
+      end_date: now,
+      api_key: apikey,
+    };
+    Joi.assert(params, neoSchema);
+    const queryString = new URLSearchParams(params);
+    const response = await axios.get(
+      `${hostname}/neo/rest/v1/feed?${queryString.toString()}`
+    );
+    const { links, ...neoData } = response.data;
+    res.json(neoData);
+  } catch (error) {
+    res.status(500).json({
+      code: "internal_server_error",
+      message: "Something went wrong",
+    });
+  }
+}
 
-module.exports = {getNeoFeed};
+module.exports = { getNeoFeed };
--- src/controllers/schemas/nasa.schema.js
+++ src/controllers/schemas/nasa.schema.js
@@ -2,7 +2,13 @@ const Joi = require('joi')
 
 const roverSchema = Joi.string().valid('opportunity', 'curiosity', 'spirit').insensitive().required()
  
+const neoSchema = Joi.object({
+    start_date: Joi.date().iso().required(),
+    end_date: Joi.date().iso().required(),
+    api_key: Joi.string().required()
+})
 
 module.exports = {
-    roverSchema
+    roverSchema,
+    neoSchema
 }
