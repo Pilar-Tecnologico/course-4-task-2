59f0a5010818702cb5e79f19cd4371a06434a310
Angel Salazar <angel.salazar@inovtech.com.mx>
2022-02-14 23:19:16 -0300

    Cambios en mars api

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -1,9 +1,10 @@
-const axios = require('axios').default;
-const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const axios = require("axios").default;
+const config = require("config");
+const { hostname, api_key } = config.get("services.nasa");
 
-async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
-};
+async function getManifest(req, res) {
+  const query = req.query;
+  const queryString = new URLSearchParams({ api_key, query });
+}
 
-module.exports = {getManifest};
+module.exports = { getManifest };
