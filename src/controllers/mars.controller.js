d13e31ea45966849b2e5de798652278c16006d8f
Alexandra <alexandrasalgan@gmail.com>
2022-02-16 08:55:39 -0300

    primer commit

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -3,7 +3,20 @@ const config = require('config');
 const {hostname, apikey} = config.get('services.nasa');
 
 async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
+    const roverName=req.params.roverName;
+    axios.get(`${hostname}/mars-photos/api/v1/manifests/${roverName}?api_key=${apikey}`)
+    .then(response=>{
+        manifest = response.data.photo_manifest;
+        manifest.last_manifest = manifest.photos.pop();
+        delete manifest.photos;
+        res.json(manifest);
+    })
+    .catch(error=>{
+        res.json({
+            code:'bad_request',
+            message:'bad request.Please check your parameters values'
+        });
+    })
 };
 
 module.exports = {getManifest};
