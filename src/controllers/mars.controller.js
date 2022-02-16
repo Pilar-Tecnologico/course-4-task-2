ac2a20671d4f75bed77985d5221da3a2f4630bc8
Angel Salazar <angel.salazar@inovtech.com.mx>
2022-02-16 19:11:50 -0300

    Mars Rover Photos

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -3,8 +3,24 @@ const config = require("config");
 const { hostname, api_key } = config.get("services.nasa");
 
 async function getManifest(req, res) {
-  const query = req.query;
-  const queryString = new URLSearchParams({ api_key, query });
+  try {
+    const query = req.params.roverName;
+    const queryString = new URLSearchParams({ api_key });
+
+    const response = await axios.get(
+      `${hostname}/mars-photos/api/v1/manifests/${query}?${queryString.toString()}`
+    );
+    const data = response.data.photo_manifest;
+    const last_manifest = data.photos.pop();
+    delete data["photos"];
+
+    res.json({ ...data, last_manifest });
+  } catch (error) {
+    res.status(400).json({
+      code: "bad_request",
+      message: "Bad request. Please check your parameters values",
+    });
+  }
 }
 
 module.exports = { getManifest };
