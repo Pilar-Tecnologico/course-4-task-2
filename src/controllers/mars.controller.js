ee98ee8e742f86a261cc0e9ce57bfcddf5ae6f29
Matias N Zamora <64152978+MatiasNZamora@users.noreply.github.com>
2022-02-18 22:20:10 -0300

    task2 complete

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -3,7 +3,29 @@ const config = require('config');
 const {hostname, apikey} = config.get('services.nasa');
 
 async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
+
+    try {
+        const roverName = req.parms.roverName;
+
+        const queryString = new URLSearchParams({
+            api_key: apikey
+        })
+
+        const response = await axios.get(
+            `${hostname}/mars-photos/api/v1/manifests/${roverName} ? ${queryString.toString()}`
+        );
+
+        const data = response.data.photo_manifest;
+        data.last_manifest = data.photo.pop();
+        delete data.photo;
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({
+        code: "bad_request",
+        message: "Bad request. Please check your parameters values"
+        });
+    }
+
 };
 
 module.exports = {getManifest};
--- src/routes/neo.js
+++ src/routes/neo.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const neoController = require('../controllers/neo.controller');
 //COMPLETE the router
-router.get('/feed', );
+router.get('/feed',neoController.getNeoFeed);
 
 
 module.exports = router;
