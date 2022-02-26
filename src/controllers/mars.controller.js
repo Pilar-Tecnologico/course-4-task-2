a91e1110ebe622e0b0d8ccc4bf5ea0cf4316a69f
matias-rivera <juanmatiasrivera@outlook.com>
2022-02-26 19:14:06 -0300

    Trabajo Práctico N°2

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -3,7 +3,23 @@ const config = require('config');
 const {hostname, apikey} = config.get('services.nasa');
 
 async function getManifest(req, res){
-    //COMPLETE WITH YOUR CODE
+    try{
+        const {roverName} = req.params;
+        
+        const query = `${hostname}/mars-photos/api/v1/manifests/${roverName}?api_key=${apikey}`;
+        const nasaResponse = await axios.get(query);
+
+        const response = nasaResponse.data.photo_manifest;
+        response.last_manifest = response.photos.slice(-1)[0];
+        delete response.photos;
+
+        res.status(200).json(response);
+    } catch(err) {
+        res.status(400).json({
+            code: "bad_request",
+            message: "Bad request. Please check your parameters values"
+        });
+    }
 };
 
 module.exports = {getManifest};
--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -3,7 +3,22 @@ const config = require('config');
 const {hostname, apikey} = config.get('services.nasa');
 
 async function getNeoFeed(req, res){
-    //COMPLETE WITH YOUR CODE
+    try{
+        const currentDate = new Date().toISOString().slice(0,10);
+
+        const query = `${hostname}/neo/rest/v1/feed?api_key=${apikey}&start_date=${currentDate}&end_date=${currentDate}`;
+        const nasaResponse = await axios.get(query);
+        
+        const response = nasaResponse.data;
+        delete response.links;
+        
+        res.status(200).json(response);
+    } catch(err) {
+        res.status(500).json({
+            code: "internal_server_error",
+            message: "Something went wrong"
+        });
+    }
 };
 
 module.exports = {getNeoFeed};
