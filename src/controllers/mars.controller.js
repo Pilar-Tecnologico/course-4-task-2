d4302b14f39c5540e33356d31c7baa08d89b869b
matias-rivera <juanmatiasrivera@outlook.com>
2022-02-27 13:06:54 -0300

    Trabajo Práctico N°3

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -1,17 +1,28 @@
 const axios = require('axios').default;
 const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const { nasa:{ apikey, hostname }, mongodb: { uri} } = config.get('services');
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(uri);
 
 async function getManifest(req, res){
     try{
-        const {roverName} = req.params;
+        const { roverName } = req.params;
         
+        // Nasa API call
         const query = `${hostname}/mars-photos/api/v1/manifests/${roverName}?api_key=${apikey}`;
         const nasaResponse = await axios.get(query);
 
+        // Modify response
         const response = nasaResponse.data.photo_manifest;
         response.last_manifest = response.photos.slice(-1)[0];
         delete response.photos;
+
+        // Insert response into database
+        await client.connect();
+        const database = client.db("nasa-api");
+        const manifest = database.collection("manifests");
+        await manifest.insertOne(response);
 
         res.status(200).json(response);
     } catch(err) {
@@ -19,6 +30,8 @@ async function getManifest(req, res){
             code: "bad_request",
             message: "Bad request. Please check your parameters values"
         });
+    } finally {
+        await client.close();
     }
 };
 
--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -1,14 +1,16 @@
 const axios = require('axios').default;
 const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const { hostname, apikey } = config.get('services.nasa');
 
 async function getNeoFeed(req, res){
     try{
         const currentDate = new Date().toISOString().slice(0,10);
 
+        // Nasa API call
         const query = `${hostname}/neo/rest/v1/feed?api_key=${apikey}&start_date=${currentDate}&end_date=${currentDate}`;
         const nasaResponse = await axios.get(query);
         
+        // Modify response
         const response = nasaResponse.data;
         delete response.links;
         
