99a271c56816173c028ea12d4488b30c88b149b9
licjavierbarrios <licjavierbarrios@hotmail.com>
2022-11-22 22:34:45 -0300

    feat: task-2 update

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -1,9 +1,18 @@
-const axios = require('axios').default;
-const config = require('config');
-const {hostname, apikey} = config.get('services.nasa');
+const { getRover } = require('../services/manifest.service');
 
-async function getManifestController(req, res, next){
-    //COMPLETE WITH YOUR CODE
-};
+async function getManifestController(req, res, next) {
+	//COMPLETE WITH YOUR CODE
+	const { roverName } = req.params;
+    
+	try {
+		const rover = await getRover(roverName);
+		res.json(rover);
+	} catch (error) {
+		res.status(400).json({
+			code: 'bad_request',
+			message: 'Bad request. Please check your parameters values',
+		});
+	}
+}
 
-module.exports = {getManifestController};
+module.exports = { getManifestController };
