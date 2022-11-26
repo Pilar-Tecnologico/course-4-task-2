103df5caa3afb13513d7745792ebb9399d8c6f8e
Ariel Cabeza <cervanteslibch@gmail.com>
2022-11-26 20:10:54 -0300

    config routes

--- src/controllers/mars.controller.js
+++ src/controllers/mars.controller.js
@@ -4,6 +4,12 @@ const {hostname, apikey} = config.get('services.nasa');
 
 async function getManifestController(req, res, next){
     //COMPLETE WITH YOUR CODE
+    try {
+        const response = await axios.get(``);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 module.exports = {getManifestController};
