4cbdb360a3a8d35e51c710cbd9fd6b3218251cea
DarioMarconi <darioenriquemarconi@gmail.com>
2022-11-27 01:27:15 -0300

    haciendo cambios en el tp2 de node

--- src/controllers/neo.controller.js
+++ src/controllers/neo.controller.js
@@ -1,9 +1,21 @@
 const axios = require('axios').default;
 const config = require('config');
 const {hostname, apikey} = config.get('services.nasa');
+const { getNeoFeedService } = require('../services/neoFeed.service');
 
 async function getNeoFeedController(req, res, next){
-    //COMPLETE WITH YOUR CODE
+        //Get start_date, end_date form Query 
+        const { start_date, end_date } = req.query;
+        try {
+            const Neo = await getNeoFeedService();
+            res.json(Neo);
+        } catch (error) {
+            res.status(500).json({
+                code: 'internal_server_error',
+                message: 'Something went wrong',
+            });
+        }
+    
 };
 
 module.exports = {getNeoFeedController};
---
+++ src/services/neoFeed.service.js
@@ -0,0 +1,19 @@
+const axios = require('axios').default;
+const config = require('config');
+const { response } = require('express');
+const { hostname, apikey } = config.get('services.nasa');
+const { DateTime } = require('luxon');
+const getNeoFeedService = async () => {
+    //Get today's date for Luxon
+    const Today = DateTime.now().toFormat('yyyy-MM-dd');
+        //return Today;
+
+    //Get Data Neo with Luxon Date and Axions
+        const { data } = await axios.get(`${hostname}/neo/rest/v1/feed?api_key=${apikey}&start_date=${Today}&end_date=${Today}`);
+    
+        //Delete data
+	delete data.links;
+	return data;
+};
+
+module.exports = { getNeoFeedService };
